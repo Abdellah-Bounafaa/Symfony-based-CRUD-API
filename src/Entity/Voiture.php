187ae0018655eeed15c65bbd9a9ff2588b710b0f
Abdellah-Bounafaa <abdllahbounafaa@gmail.com>
2024-10-14 19:32:50 +0100

    Refactor Voiture entity to add client and devis relationships

--- src/Entity/Voiture.php
+++ src/Entity/Voiture.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\VoitureRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: VoitureRepository::class)]
@@ -24,6 +26,19 @@ class Voiture
 
     #[ORM\Column(type: 'date')]
     private ?\DateTimeInterface $date_achat = null;
+
+    #[ORM\ManyToOne(targetEntity: Client::class, inversedBy: 'voitures')]
+    private ?Client $client = null;
+
+    /**
+     * @var Collection<int, Devis>
+     */
+    #[ORM\ManyToMany(targetEntity: Devis::class, mappedBy: 'voitures')]
+    private Collection $devis;
+    public function __construct()
+    {
+        $this->devis = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -71,6 +86,44 @@ class Voiture
     public function setDateAchat(\DateTimeInterface $date_achat): self
     {
         $this->date_achat = $date_achat;
+        return $this;
+    }
+
+    public function getClient(): ?Client
+    {
+        return $this->client;
+    }
+
+    public function setClient(?Client $client): self
+    {
+        $this->client = $client;
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Devis>
+     */
+    public function getDevis(): Collection
+    {
+        return $this->devis;
+    }
+
+    public function addDevi(Devis $devi): static
+    {
+        if (!$this->devis->contains($devi)) {
+            $this->devis->add($devi);
+            $devi->addVoiture($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDevi(Devis $devi): static
+    {
+        if ($this->devis->removeElement($devi)) {
+            $devi->removeVoiture($this);
+        }
+
         return $this;
     }
 }
