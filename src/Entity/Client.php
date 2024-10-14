63c5d761bea5ee548c75a922d6836c965111419e
Abdellah-Bounafaa <abdllahbounafaa@gmail.com>
2024-10-14 15:57:57 +0100

    Add Client entity and ClientRepository

---
+++ src/Entity/Client.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClientRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClientRepository::class)]
+class Client
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(type: 'date')]
+    private ?\DateTimeInterface $date_naissance = null;
+
+    #[ORM\Column(type: 'boolean')]
+    private ?bool $est_personne = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+        return $this;
+    }
+
+    public function getDateNaissance(): ?\DateTimeInterface
+    {
+        return $this->date_naissance;
+    }
+
+    public function setDateNaissance(\DateTimeInterface $date_naissance): self
+    {
+        $this->date_naissance = $date_naissance;
+        return $this;
+    }
+
+    public function getEstPersonne(): ?bool
+    {
+        return $this->est_personne;
+    }
+
+    public function setEstPersonne(bool $est_personne): self
+    {
+        $this->est_personne = $est_personne;
+        return $this;
+    }
+}
